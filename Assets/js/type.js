8f8734d76c44528be77b5bfacbe28c8ce30495fc
raajatg <raajatg@andrew.cmu.edu>
2021-05-27 17:54:13 -0400

    finished about me

--- Assets/js/type.js
+++ Assets/js/type.js
@@ -33,14 +33,23 @@ var introduction = document.getElementById('introduction');
 
 const meType = new Typewriter(introduction);
     meType.pauseFor(1000)
-    .typeString("<strong>I'm Raajat Gupta.</strong>")
-    .typeString('<br/>')
+    .typeString("<h1 style='font-size:3rem;'>I'm Raajat Gupta.</h1>")
     .pauseFor(500)
-    .typeString("I use design as a tool🔧 for building community.")
-    .typeString(" 🏚")
+    .typeString("I use research as a tool for creating connections. 🏚")
     .pauseFor(1000)
     .deleteChars(3)
     .typeString(" 🏘")
     .start();
 
+var introductionTwo = document.getElementById('introductionTwo');
 
+    const meTypeTwo = new Typewriter(introductionTwo);
+        meTypeTwo.pauseFor(14500)
+        .typeString("Let's define the <strong>future of human relationships. 👫</strong>")
+        .pauseFor(1500)
+        .deleteChars(18)
+        .typeString("<strong>-AI bonds. 🤖</strong>")
+        .pauseFor(1500)
+        .deleteChars(18)
+        .typeString("<strong>connection. 💛</strong>")
+        .start();
